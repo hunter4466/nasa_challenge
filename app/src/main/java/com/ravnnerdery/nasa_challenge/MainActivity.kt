075df832dc7671837ea4aa559e68bd9f9888fa32
Mario Chois <mario.chois.navarro@gmail.com>
2022-02-14 10:24:50 -0500

    Setup compose boilerplate app with clean architecture layers

---
+++ app/src/main/java/com/ravnnerdery/nasa_challenge/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.ravnnerdery.nasa_challenge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ravnnerdery.nasa_challenge.ui.theme.Nasa_challengeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Nasa_challengeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Nasa_challengeTheme {
+        Greeting("Android")
+    }
+}
