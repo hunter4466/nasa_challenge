81a556a1b41a398fe69bb368a2b480b6983f4f9f
Mario Chois <mario.chois.navarro@gmail.com>
2022-02-17 11:57:31 -0500

    :fire: Activity: Setup activity with Hilt configuration

--- app/src/main/java/com/ravnnerdery/nasa_challenge/MainActivity.kt
+++ app/src/main/java/com/ravnnerdery/nasa_challenge/MainActivity.kt
@@ -1,43 +1,13 @@
 package com.ravnnerdery.nasa_challenge
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ravnnerdery.nasa_challenge.ui.theme.Nasa_challengeTheme
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Nasa_challengeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Nasa_challengeTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
