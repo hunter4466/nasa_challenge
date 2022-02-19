87b6cbed6c9c2d7a0c7e737979ebc126740175f5
Mario Chois <mario.chois.navarro@gmail.com>
2022-02-19 02:48:23 -0500

    :twisted_rightwards_arrows: ViewModels: Add new useCases to view model providing

--- app/src/main/java/com/ravnnerdery/nasa_challenge/viewModels/PhotoViewModel.kt
+++ app/src/main/java/com/ravnnerdery/nasa_challenge/viewModels/PhotoViewModel.kt
@@ -2,6 +2,7 @@ package com.ravnnerdery.nasa_challenge.viewModels
 
 import androidx.lifecycle.ViewModel
 import com.ravnnerdery.data.useCases.ProvideEnlargedPhotoUseCase
+import com.ravnnerdery.data.useCases.ProvideLoadingStateUseCase
 import com.ravnnerdery.domain.models.MarsPhoto
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
@@ -9,8 +10,11 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PhotoViewModel @Inject constructor(
+    provideLoadingStateUseCase: ProvideLoadingStateUseCase,
     private val provideEnlargedPhotoUseCase: ProvideEnlargedPhotoUseCase
 ) : ViewModel() {
+    val observableLoadingState = provideLoadingStateUseCase.execute()
+
     fun enlargedPhoto(id: Long): Flow<MarsPhoto> {
         return provideEnlargedPhotoUseCase.execute(id)
     }
